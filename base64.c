0d784c4fdf9cf51108b796504ea7943e82cd4950
gekoxyz <mattegaliz@gmail.com>
2024-04-27 18:50:25 +0000

    added base64 encoding

--- base64.c
+++ base64.c
@@ -13,50 +13,32 @@ static char encoding_table[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
                                 '4', '5', '6', '7', '8', '9', '+', '/'};
 
 char* base64_encode(char* input) {
-  // int padding_size = (4 - (strlen(input) % 4)) % 4;
-  // printf("padding size: %d\n", padding_size);
-
-  // ASCII:     a         b
-  // bits:   01100001  01100010
-  // bits:   011000 010110 0010
-  // bits:   011000 010110 001000
-  // base64:   Y      W      I       =
-
-  //    a
-  // 01100001
-  // 011000 01
-  // 011000 010000
-  
-
   // 8 * n / 6 -> 4 * n / 3 gives unpadded length
   // 8 ASCII bits * input_length / 6 base64 bits
-  // And round up to the nearest multiple of 4 for padding, 
-  // and as 4 is a power of 2 can use bitwise logical operations. 
+  // And round up to the nearest multiple of 4 for padding,
+  // and as 4 is a power of 2 can use bitwise logical operations.
   // ((4 * n / 3) + 3) & ~3
   int size = ((4 * strlen(input) / 3) + 3) & ~3;
-
   char* output = malloc(size);
   unsigned char bits[size * 8];
 
   // convert input to bits
   int k = 0;
-  for (int i = 0; i < size; i++) {
+  for (int i = 0; i < strlen(input); i++) {
     for (int j = 7; j >= 0; j--) {
-      printf("%d", (((unsigned char)input[i] >> j) & 1));  // print each bit
-      bits[k] = (((unsigned char)input[i] >> j) & 1);
-      k++;
+      bits[k++] = (((unsigned char)input[i] >> j) & 1);
     }
   }
-  printf("\n");
+  for (int i = strlen(input); i < size; i++) {
+    bits[k++] = 0;
+  }
 
   unsigned char subbits[6] = {0};
   int bits_len = strlen(input) * 8;
   k = 0;
   for (int start = 0; start < bits_len; start += 6) {
     // getting the subbits
-    printf("getting subbits from %d to %d\n", start, start + 6);
     for (int i = start; i < (start + 6); i++) {
-      printf("inserting into subbits[%d] = bits[%d] = %d\n", (i - start), i, bits[i]);
       subbits[i - start] = bits[i];
     }
 
@@ -67,19 +49,19 @@ char* base64_encode(char* input) {
       // and add it to the decimal value
       decimal += subbits[i] * (1 << (5 - i));
     }
-    printf("encoding_table[%d] = %c\n", decimal, encoding_table[decimal]);
     output[k++] = encoding_table[decimal];
+  }
+  while (k < size) {
+    output[k++] = '=';
   }
   return output;
 }
 
 int main() {
-  // char* to_encode = "Hello World!";
   char* to_encode = "Hello World";
-  // char* to_encode = "a";
   printf("Encoding string: %s\n", to_encode);
   char* encoded = base64_encode(to_encode);
   printf("Encoded string: %s\n", encoded);
-  // free(encoded);
+  free(encoded);
   return 0;
 }
